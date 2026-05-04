0ea1e5a4d6957e5f82b9a6e9d7908916baf5545d
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-05-04 20:49:04 +0600

    taining page updated

--- src/app/navLinks/training/page.jsx
+++ src/app/navLinks/training/page.jsx
@@ -1,7 +1,12 @@
 import CertificatesSection from "@/components/certificates/CertificatesSection";
+import { div } from "framer-motion/client";
 
 const TrainingPage = () => {
-  return <CertificatesSection />;
+  return (
+    <div className="mt-7 mb-15">
+      <CertificatesSection />
+    </div>
+  )
 };
 
 export default TrainingPage;
